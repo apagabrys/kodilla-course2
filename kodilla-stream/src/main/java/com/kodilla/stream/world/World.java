0aef9bc1434c55bf0606b1d0c7638963bf728430
apagabrys <apagabrys@gmail.com>
2017-12-15 15:14:50 +0100

    World First

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/world/World.java
@@ -0,0 +1,17 @@
+package com.kodilla.stream.world;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+public final class World {
+    private final List<Continent> Continents = new ArrayList<>();
+
+    public List<Continent> getContinents() {
+        return Continents;
+    }
+
+
+}
